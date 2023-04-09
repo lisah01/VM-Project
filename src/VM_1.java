e96dd698bba4e9f77f468ec30118218b05d951ba
lisah01 <94252703+lisah01@users.noreply.github.com>
2023-04-09 18:14:32 -0500

    Add VM-1 operations, update driver

--- src/VM_1.java
+++ src/VM_1.java
@@ -1,3 +1,12 @@
 public class VM_1 {
+    protected void create(int p1){
+    }
+    protected void coin(float p2){}
+    protected void sugar(){}
+    protected void tea(){}
+    protected void latte(){}
+    protected void insert_cups(int n){}
+    protected void set_price(float p3){}
+    protected void cancel(){}
 
 }
